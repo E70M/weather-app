0329a50e155f3a6a9b8fe3d828a1d40507d4ae1b
Eitan Myron <eitanmyron@outlook.com>
2021-06-25 08:33:19 -0700

    Cleanup (#32)

--- src/services/temperature-converter/index.js
+++ src/services/temperature-converter/index.js
@@ -1,34 +1,34 @@
 function conversion(temp, curr, desired) {
-	if (curr !== 'kelvin') {
-		temp = 273.15 + (curr === 'fahrenheit' ? (temp - 32.0) / 1.8 : temp);
-	}
-	if (desired !== 'kelvin') {
-		temp -= 273.15;
-		if (desired === 'fahrenheit') {
-			temp = 1.8 * temp + 32.0;
-		}
-	}
-	return temp;
+  if (curr !== 'kelvin') {
+    temp = 273.15 + (curr === 'fahrenheit' ? (temp - 32.0) / 1.8 : temp);
+  }
+  if (desired !== 'kelvin') {
+    temp -= 273.15;
+    if (desired === 'fahrenheit') {
+      temp = 1.8 * temp + 32.0;
+    }
+  }
+  return temp;
 }
 
 const temperatureConverterService = (temp, curr, desired) => {
-	temp['units'] = desired;
-	if (curr === desired) {
-		return temp;
-	}
-	if (temp.temperature) {
-		temp.temperature = conversion(temp.temperature, curr, desired);
-	}
-	if (temp.feels_like) {
-		temp.feels_like = conversion(temp.feels_like, curr, desired);
-	}
-	if (temp.temp_max) {
-		temp.temp_max = conversion(temp.temp_max, curr, desired);
-	}
-	if (temp.temp_min) {
-		temp.temp_min = conversion(temp.temp_min, curr, desired);
-	}
-	return temp;
+  temp['units'] = desired;
+  if (curr === desired) {
+    return temp;
+  }
+  if (temp.temperature) {
+    temp.temperature = conversion(temp.temperature, curr, desired);
+  }
+  if (temp.feels_like) {
+    temp.feels_like = conversion(temp.feels_like, curr, desired);
+  }
+  if (temp.temp_max) {
+    temp.temp_max = conversion(temp.temp_max, curr, desired);
+  }
+  if (temp.temp_min) {
+    temp.temp_min = conversion(temp.temp_min, curr, desired);
+  }
+  return temp;
 };
 
 export default temperatureConverterService;
